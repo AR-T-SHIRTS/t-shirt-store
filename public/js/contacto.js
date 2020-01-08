dbda1cf3f89ca561cafa9b6d31e1f44795cf843b
Damian Eduardo Dominguez <alu0101138183@ull.edu.es>
2020-01-08 11:22:16 +0000

    Agregando script en contacto

---
+++ public/js/contacto.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.submit').click(function (event) {
+        event.preventDefault()
+
+        var email = document.getElementById('email').nodeValue;
+        var mensaje = document.getElementById('mensaje').nodeValue;
+    })
+})
