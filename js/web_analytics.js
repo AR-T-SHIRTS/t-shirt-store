eead3581af1906d841190f61600aa3e2ea2c55ce
Damian Eduardo Dominguez <alu0101138183@ull.edu.es>
2020-01-14 17:06:39 +0000

    Arreglando ubicacion de ficheros

--- js/web_analytics.js
+++
@@ -1,9 +0,0 @@
-const analytics = firebase.analytics();
-
-async function signIn() {
-    const user = await firebase.auth().signInWithEmailAndPassword(inputEmail, inputPassword);
-    const method = user.credential.signInMethod;
-
-    analytics.logEvent('login', { method });
-// :)))))))))
-}
